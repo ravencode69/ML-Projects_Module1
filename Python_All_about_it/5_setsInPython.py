dd515863b8318c52d8ad19f50d5f48b4129d5884
Raven <1906569@kiit.ac.in>
2022-02-01 23:58:41 +0530

    Sets in python

---
+++ Python_All_about_it/5_setsInPython.py
@@ -0,0 +1,12 @@
+# cant have any duplicates also unordered - cant iterate
+set2 = {"hello my name is richa", "i hope you're fine"}
+set2.add("sunflower")
+set2.add("sunflower")
+# print(set2)
+
+l = [1, 2, 2, 2, 4, 5, 6, 7]
+
+no_duplicate = set(l)  # made list a set to remove duplicates
+print(no_duplicate)
+no_duplicate = list(no_duplicate)  # casting back to list
+print(no_duplicate)
