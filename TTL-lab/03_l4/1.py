76b519f80d0bea1aee71a53505ef21f40b05ec92
Raven <1906569@kiit.ac.in>
2022-03-06 03:36:34 +0530

    del

--- TTL-lab/03_l4/1.py
+++
@@ -1,9 +0,0 @@
-
-a = 10
-b = 14
-c = 12
-print(max(a, b, c))
-
-# 2
-txt = "Hello World"[::-1]
-print(txt)
--- TTL-lab/03_l4/3.py
+++
@@ -1,6 +0,0 @@
-# Write a Python function that takes a list and returns a new list with unique
-# elements of the first list.
-
-l = [1, 2, 3, 3, 3, 3, 4, 5]
-l = set(l)
-print(l)
