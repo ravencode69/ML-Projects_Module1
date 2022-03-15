0b14b2173c67043ccddc1dc4a458184e551fcb72
Raven <1906569@kiit.ac.in>
2022-03-16 02:08:56 +0530

    Zip

--- Zip Functions/1_zip.py
+++ Zip Functions/1_zip.py
@@ -1,5 +1,20 @@
 # takes corrosponding element and glue them together
-list1 = {1, 2, 3, 4, 5}
-list2 = {'one', 'two', 'three', 'four'}
+list1 = [1, 2, 3, 4, 5]
+list2 = ['one', 'two', 'three', 'four']
 
-zip = list(zip(list1, list2))
+zip1 = list(zip(list1, list2))
+
+print(zip1)
+# [(1, 'two'), (2, 'three'), (3, 'four'), (4, 'one')
+
+unzip = list(zip(*zip1))
+print(unzip)
+
+items = ['apple', 'banana', 'orange']
+count = [3, 6, 5]
+pr = [0.99, 0.23, 0.43]
+
+sentences = []
+for(item, count, pr) in zip(items, count, pr):
+    sentences.append = 'I buy'+count+''+item+'s'+pr+'.'
+print(sentences)
